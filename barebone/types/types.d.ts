1cdfa9f1c6d53215596eb16408d06dfe4f5ace43
Kenny Onishi <kenny619@gmail.com>
2024-07-06 10:29:42 +0900

    context

--- barebone/types/types.d.ts
+++ barebone/types/types.d.ts
@@ -1,10 +1,36 @@
 type tableData = { [key: string]: string | number }[];
 type payload = {
+	crudId: number;
+	mode: mode;
 	id: number;
 	colName: string;
 	newVal: string;
 	oldVal: string;
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	hooks: { [key: string]: (...args: any) => void };
-	backupData?: tableData;
+};
+
+type mode =
+	| "create"
+	| "update"
+	| "delete"
+	| "undoCreate"
+	| "undoUpdate"
+	| "undoDelete";
+type Context = null | singleColumnCRUDTableContext;
+
+type singleColumnCRUDTableContext = {
+	crudId: number;
+	setCrudId: React.Dispatch<React.SetStateAction<number>>;
+	tableData: tableData;
+	dispatch: React.Dispatch<mode>;
+	editId: number;
+	setEditId: React.Dispatch<React.SetStateAction<number>>;
+	textInput: string;
+	setTextInput: React.Dispatch<React.SetStateAction<string>>;
+	showToast: boolean;
+	setShowToast: React.Dispatch<React.SetStateAction<boolean>>;
+	toastElements: never[];
+	setToastElements: React.Dispatch<React.SetStateAction<never[]>>;
+	crudQueue: payload[];
+	setCrudQueue: React.Dispatch<React.SetStateAction<payload[]>>;
+	tableDataRef: React.MutableRefObject<tableData>;
 };
